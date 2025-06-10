1f0b7f36c53f761f8091cef85faf49105f8eafb7
Youssof Kandil <yaskm2002@gmail.com>
2025-06-10 18:28:52 +0300

    cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
